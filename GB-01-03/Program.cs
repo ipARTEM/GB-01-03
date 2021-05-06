ff6ba17dddbefc5bb0477f2a2fb4ad47dae2f262
Artem <a.khimin@yandex.ru>
2021-05-06 19:43:03 +0300

    Добавьте файлы проекта.

---
+++ GB-01-03/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк : ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите количество столбцов : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[m, n];
+            Random random = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(100);
+                   
+                }
+            }
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    for (int b = 0; b < j;b++ )
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine(array[i,j]+" ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
---
+++ Task02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа телефонный справочник");
+
+            Random random = new Random();
+
+            int number = random.Next(9999999);
+
+            string[] name = new string[] {"Саша","Петя", "Вася", "Коля", "Толя" }; 
+
+
+            string[,] phoneDirectory=new string [5,2];
+
+            for (int i = 0; i < phoneDirectory.GetLength(0); i++)
+            {
+                for (int j = 0; j < phoneDirectory.GetLength(1); j++)
+                {
+                    phoneDirectory[i, j] = Convert.ToString( random.Next(9999999));
+                    Console.WriteLine(phoneDirectory[i, j]);
+
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
