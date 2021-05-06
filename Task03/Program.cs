341d88a26ed3766e4178588b0d84a019478b064b
Artem <a.khimin@yandex.ru>
2021-05-07 00:26:06 +0300

    Доделал 4е задание

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -26,3 +26,4 @@ namespace Task03
         }
     }
 }
+                                                       
---
+++ Task04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var battle = new string[10, 10];
+            var chars = "XOOO";
+            var random = new Random();
+
+            for (int i = 0; i < battle.GetLength(0); i++)
+            {
+                Console.Write(" ");
+                for (int j = 0; j < battle.GetLength(1); j++)
+                {
+                    Console.Write(chars[random.Next(chars.Length)]);
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
